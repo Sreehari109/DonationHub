653066cb7a72b245ea2456b2dc7f908a1a9cfcd0
Sreehari109 <sreeharim109@gmail.com>
2021-06-23 23:04:08 +0530

    my first commit project1

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Heading from "./Heading"
 import Cards from "./Cards"
 import Contactus from "./Contactus"
 import Aboutus from "./Aboutus"
+import Error from './Error';
 import Home from "./Home"
 import Navbar from './Navbar';
 import Wp from "./Wp";
@@ -17,6 +18,7 @@ function App() {
   <Route exact path="/Contactus" component={Contactus}/>
   <Route exact path="/Aboutus" component={Aboutus} />
   <Route exact path="/Wp" component={Wp} />
+  <Route component={Error}/>
 </Switch>
 <Heading/>
 <Carousel/>
---
+++ src/Error.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function Error(){
+    return(
+<>
+<h1>Page Not Found , Error</h1>
+<p>Sorry for the inconvenience</p>
+</>
+    );
+}
+export default Error;
